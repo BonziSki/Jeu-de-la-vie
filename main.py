e859899769d5f540f055173ae3b78456d8747a27
BonziSki <yanis.zribi@gmail.com>
2022-09-02 22:52:24 +0200

    ajout des touches 'm' et 'l' pour changer la taille de la grille

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ width = 720
 cell_h = height / len(table_n0)
 cell_w = width / len(table_n0[0])
 
-tick_rate = 4
+tick_rate = 6
 
 pause = 0
 
@@ -75,6 +75,10 @@ black_surface.fill('black')
 
 def cell_alive(table, x, y) :
     count = 0
+
+
+    print("x : ", x, "| y : ", y)
+
 
     if x == 0 :
         if y == 0 :
@@ -125,6 +129,7 @@ def cell_alive(table, x, y) :
                     if not (i == 0 and j == 0) :
                         if table[y + i][x + j] == 1 :
                             count += 1
+
         elif y == (height / cell_h) - 1 :
             for i in [-1, 0] :
                 for j in [-1, 0, 1] :
@@ -147,12 +152,12 @@ def cell_alive(table, x, y) :
 while True :
     #Event
     for event in pygame.event.get() :
+
         #Fermeture du programme
         if event.type == pygame.QUIT :
             pygame.quit()
             exit()
         
-
         #Clics de souris
         if event.type == pygame.MOUSEBUTTONDOWN :
             if pause == 1 :
@@ -162,14 +167,10 @@ while True :
                 cell_col = int((mouse_x - (mouse_x % cell_w)) / cell_w)
                 cell_row = int((mouse_y - (mouse_y % cell_h)) / cell_h)
 
-                print("x : ",cell_col)
-                print("y : ",cell_row)
-
                 if table_n0[cell_row][cell_col] == 0 :
                     table_n0[cell_row][cell_col] = 1
                 else :
                     table_n0[cell_row][cell_col] = 0
-        
         
         #Touches de clavier
         if event.type == pygame.KEYDOWN :
@@ -198,7 +199,42 @@ while True :
                     for i, row in enumerate(table_n0) :
                         for j, cell in enumerate(row) :
                             table_n0[i][j] = random.randint(0,1)
+            
+            #Agrandissement de la fenetre
+            if event.unicode == 'm' :
+                if pause == 1 :
+                    for i, row in enumerate(table_n0) :
+                        table_n0[i].append(0)
+                        table_n1[i].append(0)
+                    table_n0.append([])
+                    table_n1.append([])
+                    for i, row in enumerate(table_n0[0]) :
+                        table_n0[len(table_n0[0]) - 1].append(0)
+                        table_n1[len(table_n0[0]) - 1].append(0)
 
+                    #Calcul de la taille des nouvelles cellules
+                    cell_h = height / len(table_n0)
+                    cell_w = width / len(table_n0[0])
+
+                    black_surface = pygame.Surface((cell_h, cell_w))
+                    black_surface.fill('black')
+            
+
+            #Rapetissement de la fenetre
+            if event.unicode == 'l' :
+                if pause == 1 :
+                    for i, row in enumerate(table_n0) :
+                        table_n0[i].pop()
+                        table_n1[i].pop()
+                    table_n0.pop()
+                    table_n1.pop()
+
+                    #Calcul de la taille des nouvelles cellules
+                    cell_h = height / len(table_n0)
+                    cell_w = width / len(table_n0[0])
+
+                    black_surface = pygame.Surface((cell_h, cell_w))
+                    black_surface.fill('black')
 
     #Reset de la fenetre
     window.fill('white')
